149f6947a4cda957c2a19fbc7c576893f3a7817a
Luiza Rodrigues <luizars@id.uff.br>
2024-02-25 16:02:25 -0300

    feat: sidebar component profile

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,19 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
-            <img className={styles.cover} src="https://symbols.vn/wp-content/uploads/2021/12/Hinh-bau-troi-dem-dep-Anime-vang-ve.jpg" width="500px"></img>
+            <img className={styles.cover} src="https://symbols.vn/wp-content/uploads/2021/12/Hinh-bau-troi-dem-dep-Anime-vang-ve.jpg" alt="Cover Image" width="500px"></img>
 
             <div className={styles.profile}>
+                <img src="https://github.com/Luizenys.png" />
                 <strong>Monkey D. Luffy</strong>
                 <span>Pirate King</span>
             </div>
 
             <footer>
-                <a href="#">Editar seu Perfil</a>
+                <a href="#"><PencilLine size={18} />Editar seu Perfil</a>
             </footer>
         </aside>
     )
