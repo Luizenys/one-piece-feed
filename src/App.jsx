21343a6d05cdcae42d9e39b5e66fdd680333c80c
Luiza Rodrigues <luizars@id.uff.br>
2024-02-24 23:16:05 -0300

    feat: global CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
+import { Post } from "./Post"
+import { Header } from "./components/Header"
+import './global.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <>
-      <p>Hello World</p>
-    </>
+    <div>
+      <Header></Header>
+      <Post 
+        author="Monkey D. Luffy" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate magni neque quisquam dignissimos enim expedita illo consectetur eius? Nesciunt ipsam impedit perspiciatis iusto asperiores odio debitis eaque porro fugit cupiditate" 
+      />
+    </div>
   )
 }
-
-export default App
